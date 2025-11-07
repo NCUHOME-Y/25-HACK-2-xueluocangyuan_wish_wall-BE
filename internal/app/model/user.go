2cb939aebe3bf8ba007e78162b3f4516959b3918
Hao Tang <2482904008@qq.com>
2025-11-07 15:50:12 +0800

    feat:新增奶农部分在user.go和wish.go部分所需字段,go.sum里有新增

---
+++ internal/app/model/user.go
@@ -0,0 +1,15 @@
+// 25-HACK-2-xueluocangyuan_wish_wall-BE/internal/app/model/user.go
+package model
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Username string `gorm:"uniqueIndex;size:64;not null" json:"username"` // 用户名，唯一且非空
+	Password string `gorm:"not null;size:255" json:"-"`                   // 密码，非空
+	Nickname string `gorm:"size:64" json:"nickname"`                      // 昵称
+	Avatar   string `gorm:"size:255" json:"avatar"`
+
+	Role                 string `gorm:"size:16;default:'user'" json:"role"` // 角色，默认为"user"
+	HasCompletedV2Review bool   `gorm:"default:false" json:"-"`
+}
